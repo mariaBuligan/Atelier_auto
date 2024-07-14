5ef4527745d003d1992efbc5755a5c5868818132
mariaBuligan <mariabuligan13@yahoo.com>
2024-07-14 13:34:39 +0300

    final masini

--- main.cpp
+++ main.cpp
@@ -3,22 +3,46 @@
 #include <vector>
 #include <typeinfo>
 #include "angajati.hpp"
+#include "masini.hpp"
   
 using namespace std;  
-  
+
+template <class T>
+void afisare_vector(vector<T> &v){
+      for(auto it:v) it->afisare();
+}
+
 int main() {
 
- vector<Angajat*> v;
- adaugare_angajat(v);
- adaugare_angajat(v);
- for(auto it : v) {
-      it->afisare();
-} 
-      editare_angajat(1,v);
+ vector<Angajat*> employes;
+ vector<Masini*> cars;
 
-       for(auto it : v) {
-      it->afisare();
-} 
+ Masini m(123,100000,2009,true);
+ Standard *volvo = new Standard(m,true);
+ Autobuz *audi = new Autobuz (m,12);
+ Camion *cam = new Camion(m,2.5);
+
+ cars.push_back(volvo);
+ cars.push_back(audi);
+ cars.push_back(cam);
+
+ Angajat a("Buligan","Maria",13,5,2003,1,7,2022);
+ Mecanic *mec = new Mecanic(a);
+ employes.push_back(mec);
+
+ afisare_vector(employes);
+ afisare_vector(cars);
+
+ for(auto it:cars){
+      cout<<it->polita()<<" "<<it->polita_DISCOUNT()<<endl;
+ }
+
+
+
+
+/*  adaugare_angajat(v);
+ adaugare_angajat(v); */
+
 
       return 0;
 }
---
+++ masini.hpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+time_t now = time(0);
+tm* lct =localtime(&now);
+
+class Masini{
+ protected:
+    int ID;
+    float km;
+    int an;
+    bool Diesel;
+ public:
+    Masini():km(0),an(0){};
+    Masini(int id, float k, int a, bool D):ID(id),km(k),an(a),Diesel(D){}
+    virtual void afisare(){
+        cout<<"Masina cu ID-ul:"<<ID<<endl;
+        cout<<km<<" km, vechime, din anul "<<an<<", cu motor de tip:";
+        if(Diesel)cout<<"Diesel"<<endl;
+        else cout<<"NU Diesel"<<endl;
+    }
+    virtual float polita(){return 0;}
+    virtual float polita_DISCOUNT(){return 0;}
+};
+
+class Standard: public Masini{
+    private:
+      bool Manuala;
+    public:
+        Standard():Masini(){};
+        Standard(int id, float k,int a,bool D,bool M):Masini(id,k,a,D),Manuala(M){}
+        Standard(const Masini &m, bool M):Masini(m),Manuala(M){};
+        void afisare(){
+            Masini::afisare();
+            cout<<"Tip de transmisie:";
+            if(Manuala)cout<<" Manuala"<<endl;
+            else cout<<"Automata"<<endl;
+        }
+        float polita(){
+            int vechime = lct->tm_year +1900 - an;
+            float polita = vechime *100.0;
+            if(Diesel)polita+=500;
+            if(km>=200000)polita+=500;
+            return polita;
+        }
+         float polita_DISCOUNT(){
+            return polita()-polita()*0.05;
+        }
+};
+
+class Autobuz: public Masini{
+    private:
+      int nr_loc;
+    public:
+       Autobuz():Masini(){};
+        Autobuz(int id, float k,int a,bool D,int nr):Masini(id,k,a,D),nr_loc(nr){}
+        void afisare(){
+            Masini::afisare();
+            cout<<"Numar de locuri:"<<nr_loc<<endl;
+        }
+        Autobuz(const Masini &m, int nr):Masini(m),nr_loc(nr){};
+        float polita(){
+            int vechime = lct->tm_year +1900 - an;
+            float polita = vechime *200.0;
+            if(Diesel)polita+=1000;
+            if(km>=100000 && km<200000)polita+=500;
+            if(km>=200000)polita+=1000;
+            return polita;
+        }
+         float polita_DISCOUNT(){
+            return polita()-polita()*0.1;
+        }
+};
+
+class Camion: public Masini{
+    private:
+      float tonaj;
+    public:
+        Camion():Masini(){};
+        Camion(int id, float k,int a,bool D,float t):Masini(id,k,a,D),tonaj(t){}
+        Camion(const Masini &m, float t):Masini(m),tonaj(t){};
+        void afisare(){
+            Masini::afisare();
+            cout<<"Tonajul:"<<tonaj<<endl;
+        }
+        float polita(){
+            int vechime = lct->tm_year +1900 - an;
+            float polita = vechime *300.0;
+            if(km>=800000)polita+=700;
+            return polita;
+        }
+        float polita_DISCOUNT(){
+            return polita()-polita()*0.15;
+        }
+};
